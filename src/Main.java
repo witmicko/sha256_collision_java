8394a6d2ce44607cbf7dce94466c263189193c5d
Michal Ogrodniczak <mjogrodniczak@gmail.com>
2014-11-12 11:36:26 +0000

    moved to Trove primitive long int hashmap. Works fully now

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import gnu.trove.map.hash.TLongIntHashMap;
+
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
@@ -10,30 +12,29 @@ public class Main {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-
-        Map<Long, Integer> map = new HashMap<>(41729761);
+        TLongIntHashMap map = new TLongIntHashMap();
         boolean found = false;
         byte[] digest;
         byte[] last50;
-        int i = 0;
         int max = (int)Math.pow(2,30);
-        StringBuilder sb = new StringBuilder();
-        while (!found) {
-            sb.setLength(0);
-            ++i;
-            md.update(String.valueOf(i).getBytes());
-            digest = md.digest();
-            last50 = Arrays.copyOfRange(digest, digest.length - 8, digest.length);
-            last50[0]&=0;
-            last50[1]&=3;
-            ByteBuffer bb = ByteBuffer.wrap(last50);
-            long l = bb.getLong();
-
-            Integer val = map.put(l, i);
-            if (val != null) {
-                System.out.println(val + " same as " + i + " \t\t" + Long.toBinaryString(l));
-            }
-
+        int i = 0;
+        int index;
+        ByteBuffer bb;
+        long l;
+        while (!found && i < max) {
+                ++i;
+                md.update(String.valueOf(i).getBytes());
+                digest = md.digest();
+                last50 = Arrays.copyOfRange(digest, digest.length - 8, digest.length);
+                last50[0] = 0;
+                last50[1] &= 3;
+                bb = ByteBuffer.wrap(last50);
+                l = bb.getLong();
+                index = map.put(l, i);
+                if (index > 0) {
+                    found = true;
+                    System.out.println(index + " same as " + i + " \t\t" + Long.toBinaryString(l));
+                }
         }
     }
 }
